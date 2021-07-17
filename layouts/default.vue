0ad3658b66363bb06704b14fa058799d41ccf049
sciemesfin55@gmail.com <sciemesfin55@gmail.com>
2021-07-18 00:33:35 +0300

    update changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -30,8 +30,45 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 .main-root {
   margin-top: 64px !important;
+}
+.h-0 {
+  width: 100%;
+  color: #515fbc;
+  font-size: 26px;
+}
+.h-1 {
+  color: #1f2430;
+  font-size: 48px;
+  margin-bottom: 40px;
+}
+.h-2 {
+  color: #1f2430;
+  font-size: 40px;
+  margin-bottom: 40px;
+}
+.h-3 {
+  color: #1f2430;
+  font-size: 30px;
+  margin-bottom: 40px;
+}
+.b {
+  font-weight: 700 !important;
+  color: black;
+}
+.b1 {
+  font-weight: 500 !important;
+  color: #032d60 !important;
+}
+.black {
+  color: black;
+}
+.lgy {
+  background-color: lightgrey !important;
+}
+.ndec {
+  text-decoration: none;
 }
 </style>
